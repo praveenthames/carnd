b2f7fd92570e2ab3f2837c6bfc3ba2a94f741b0c
Praveen Subramanian <praveen.subramanian@veritas.com>
2017-01-15 18:17:02 -0800

    NVidia changes

--- project-behavioral-cloning.py
+++ project-behavioral-cloning.py
@@ -22,6 +22,7 @@ from keras.utils import np_utils
 
 from keras.models import model_from_json
 from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.advanced_activations import ELU
 from keras.wrappers.scikit_learn import KerasRegressor
 
@@ -91,57 +92,97 @@ X_val = X_val.astype('float32')
 
 
 input_shape = (image_rows, image_columns, image_channels)
-model = Sequential()
-model.add(Convolution2D(24,5,5, input_shape=input_shape, subsample = (2,2),
-                    border_mode='same',
-                    name='conv1', init='he_normal'))
-model.add(ELU())
+# model = Sequential()
+# model.add(Convolution2D(24,5,5, input_shape=input_shape, subsample = (2,2),
+#                     border_mode='same',
+#                     name='conv1', init='he_normal'))
+# model.add(ELU())
+# 
+# model.add(Convolution2D(36,5,5, subsample = (2,2),
+#                     border_mode='same',
+#                     name='conv2', init='he_normal'))
+# model.add(ELU())
+# model.add(Convolution2D(48,5,5, subsample = (2,2),
+#                     border_mode='valid',
+#                     name='conv3', init='he_normal'))
+# model.add(ELU())
+# model.add(Convolution2D(64,3,3, subsample = (1,1),
+#                     border_mode='valid',
+#                     name='conv4', init='he_normal'))
+# model.add(ELU())
+# model.add(Convolution2D(64,3,3, subsample = (1,1),
+#                     border_mode='valid',
+#                     name='conv5', init='he_normal'))
+# model.add(ELU())
+# model.add(Flatten())
+# model.add(Dense(100,name='hidden1', init='he_normal'))
+# model.add(ELU())
+# model.add(Dense(50,name='hidden2', init='he_normal'))
+# model.add(ELU())
+# model.add(Dense(10,name='hidden3',init='he_normal'))
+# model.add(ELU())
+# model.add(Dense(1, name='output', init='he_normal'))
 
-model.add(Convolution2D(36,5,5, subsample = (2,2),
-                    border_mode='same',
-                    name='conv2', init='he_normal'))
-model.add(ELU())
-model.add(Convolution2D(48,5,5, subsample = (2,2),
-                    border_mode='valid',
-                    name='conv3', init='he_normal'))
-model.add(ELU())
-model.add(Convolution2D(64,3,3, subsample = (1,1),
-                    border_mode='valid',
-                    name='conv4', init='he_normal'))
-model.add(ELU())
-model.add(Convolution2D(64,3,3, subsample = (1,1),
-                    border_mode='valid',
-                    name='conv5', init='he_normal'))
-model.add(ELU())
-model.add(Flatten())
-model.add(Dense(100,name='hidden1', init='he_normal'))
-model.add(ELU())
-model.add(Dense(50,name='hidden2', init='he_normal'))
-model.add(ELU())
-model.add(Dense(10,name='hidden3',init='he_normal'))
-model.add(ELU())
-model.add(Dense(1, name='output', init='he_normal'))
+def creat_model_nvidia():
+    
+    model = Sequential()
+
+    model.add(Convolution2D(24, 5, 5, input_shape=input_shape, subsample=(2, 2), border_mode="valid", activation='relu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="valid", activation='relu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="valid", activation='relu'))
+    model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid", activation='relu'))
+    model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid", activation='relu'))
+    
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Activation('relu'))
+
+    model.add(Dense(1164))
+    model.add(Dropout(0.5))
+    model.add(Activation('relu'))
+    
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+#     model.compile(optimizer=Adam(lr=0.0001), loss="mse")
+
+    return model
+
+model = creat_model_nvidia()
 adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 model.summary()
 model.compile(optimizer=adam, loss='mse')
 
 
-def batchgen(features,output):
-    num_features = len(features)
-    for i in num_features:
-        yield (features[i],output[i])
-# history = model.fit(X_train, y_train,
-#                     batch_size=batch_size, nb_epoch=nb_epoch,
-#                     verbose=1, validation_data=(X_val, y_val))
-# score = model.evaluate(X_val, y_val, verbose=0)
-# print('Test score:', score[0])
-# print('Test accuracy:', score[1])
-history = model.fit_generator(batchgen(X_train, y_train), samples_per_epoch = 20, nb_epoch = nb_epoch,
-                    verbose=1, max_q_size = 10,
-                     pickle_safe=False)
+def batchgen(images,output):
+    num_images = len(images)
+    print("num_images = ",num_images)
+    for i in range(0,num_images,100):
+        yield (images[i:i + 100],output[i:i + 100])
+      
+i = 1  
+batch = batchgen(X_train,y_train)
+for x,y in batch:
+    print("Dimension 1 of image 1",len(x))
+    print("************")
+    print("Dimension 2 of image 1",len(x[0]))
+    print("************")
+    print("Dimension 3 of image 1",len(x[0][0]))
+    print("************")
+    print(y)
+    i = i + 1
+    if(i > 1):
+        break
+        
+
+history = model.fit_generator(batchgen(X_train, y_train), samples_per_epoch = 100, nb_epoch = nb_epoch,
+                     verbose=1, max_q_size = 10,
+                      pickle_safe=False)
 score = model.evaluate(X_val, y_val, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+print('Test score:', score)
+# print('Test accuracy:', score[1])
 
 # fix random seed for reproducibility
 # seed = 7
